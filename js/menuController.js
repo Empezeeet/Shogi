884c994c5ad2a000abae12dd4f436881a322fb0d
Mateusz <pawelkomateusz@gmail.com>
2025-05-29 14:20:52 +0200

    Theme controller for menu.

--- js/menuController.js
+++ js/menuController.js
@@ -8,4 +8,12 @@ function changeTheme() {
     let selector = document.getElementById("theme-select");
     theme = selector.value;
     window.history.replaceState(null, null, '?theme=' + theme);
+    if (theme === 'dark') {
+        document.querySelector(':root').style.setProperty('--bg-Color', '#121212');
+        document.querySelector(':root').style.setProperty('--text-Color', '255, 255, 255');
+    } else if (theme === 'light') {
+        document.querySelector(':root').style.setProperty('--bg-Color', '#fff');
+        document.querySelector(':root').style.setProperty('--text-Color', '18, 18, 18');
+    }
+
 }
